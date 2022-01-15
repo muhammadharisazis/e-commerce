36e1f77397460ade14f8902c8a7cbf218e870ee1
ibrahimathano01 <97752119+ibrahimathano01@users.noreply.github.com>
2022-01-15 16:17:59 +0800

    ubah index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<meta http-equiv="Content-Type"'.' content="text/html; charset=utf8"/>
+<meta http-equiv="Content-Type"'.' content="text/html; charset="utf8"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
 <body>
